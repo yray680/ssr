dfd692cdb15d10efb603cf07ede670386370e1c6
chuccp <cooge123@gmail.com>
2022-02-18 12:06:36 +0800

    2

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/chuccp/cokeV2ray
+
+go 1.17
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/pires/go-proxyproto v0.6.1 // indirect
+	github.com/v2fly/v2ray-core/v5 v5.0.2 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/v2fly/v2ray-core/v5/main/commands"
+	"github.com/v2fly/v2ray-core/v5/main/commands/base"
+	"os"
+)
+
+func main() {
+
+	os.Args = []string{"run","run"}
+
+	base.RootCommand.Long = "A unified platform for anti-censorship."
+	base.RegisterCommand(commands.CmdRun)
+	base.RegisterCommand(commands.CmdVersion)
+	base.RegisterCommand(commands.CmdTest)
+	base.SortLessFunc = runIsTheFirst
+	base.SortCommands()
+	base.Execute()
+}
+
+func runIsTheFirst(i, j *base.Command) bool {
+	left := i.Name()
+	right := j.Name()
+	if left == "run" {
+		return true
+	}
+	if right == "run" {
+		return false
+	}
+	return left < right
+}
